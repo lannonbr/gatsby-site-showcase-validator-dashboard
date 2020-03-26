8aa7ed3d21de2135cc192600cb01dd97ccb324f1
Benjamin Lannon <benjamin@lannonbr.com>
2020-03-25 22:21:36 -0400

    parse entries and write out to a json file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
@@ -60,9 +62,44 @@ const { Octokit } = require("@octokit/rest");
     }
   }).then(resp => resp.text());
 
-  const entries = text
+  let entries = text
     .split("\r\n")
     .filter(line => line.includes("[Notice]") || line.includes("[Err]"));
 
-  console.log(entries);
+  entries = entries.map(entry => {
+    // let time, type, name, url, rest;
+
+    let [time, rest1] = entry.split(" [");
+    let [type, rest2] = rest1.split("]: ");
+    let [name, rest3, source_url] = rest2.split(" (");
+    let [url, rest4] = rest3.split(") ");
+
+    let reason;
+
+    if (rest4.startsWith("is not")) {
+      reason = rest4;
+    } else if (rest4.startsWith("provided a ")) {
+      reason = rest4;
+    } else {
+      reason = rest4.split("reason: ")[1];
+    }
+
+    if (source_url) {
+      source_url = source_url.slice(0, -1);
+    }
+
+    return {
+      time,
+      type,
+      name,
+      url,
+      reason,
+      source_url
+    };
+  });
+
+  fs.writeFileSync(
+    path.join(__dirname, "src", "data", "entries.json"),
+    JSON.stringify(entries)
+  );
 })();
